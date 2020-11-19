4aeb2e7aefee91998d49cd5e52ddb477df9627bb
nohaderf <freda.hon@gmail.com>
2020-11-19 10:38:28 -0500

    not done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
 console.log('%c HI', 'color: firebrick')
+
+const dogContainer = document.getElementById("dog-image-container")
+
+fetch('https://dog.ceo/api/breeds/image/random/4')
+    .then(resp => resp.json())
+    .then(data => displayDogs(data.message)) 
+
+
+function displayDogs(doggos){
+    
+    // for(dog of doggos){
+    //     const image = document.createElement("img")
+    //     image.src = dog
+    //     dogContainer.append(image)
+    // }
+
+    doggos.forEach(dog => {
+        const dogImage = document.createElement("img")
+        dogImage.src =  dog
+        dogContainer.append(dogImage)
+    })
+}
+
+fetch('https://dog.ceo/api/breeds/list/all')
+    .then(resp => resp.json())
+    .then(data => fetchBreed(data.message))
+
+const dogBreed = document.querySelector("#dog-breeds")
+
+function fetchBreed(breeds){
+    for (breed in breeds){
+        const dogLi = document.createElement("li")
+        dogLi.textContent = breed
+        dogBreed.append(dogLi) 
+    }
+}
+
+dogBreed.addEventListener("click", function(event){
+    const li = dogBreed.querySelector("li")
+
+    if (event.target.textContent && event.target.li == "LI") {
+        li.style.color = "red"
+    }
+})
